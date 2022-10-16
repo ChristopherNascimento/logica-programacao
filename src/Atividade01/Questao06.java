454f309820871126bce5968c97b08779bc6bf8a5
ChristopherNascimento <christopher.nascimento12@gmail.com>
2022-10-16 13:43:16 -0300

    Questao06

---
+++ src/Atividade01/Questao06.java
@@ -0,0 +1,35 @@
+package Atividade01; 
+    
+import java.util.Scanner;
+
+public class Questao06 {
+
+public static void main(String[] args) throws Exception {
+
+    Scanner ler = new Scanner(System.in);
+
+    System.out.println("Valor total da conta");
+    
+        float ValorTotal = ler.nextFloat();
+    
+    System.out.println("Total de pessaos que irão dividir a conta");
+        
+        int Pessoas = ler.nextInt();
+
+        float ValorPago = ValorTotal / Pessoas;
+        
+    System.out.println("o valor que cada pessoa vai pagar é de R$"+ValorPago);
+    
+
+    
+    
+
+    
+
+
+
+
+
+
+    }
+}
